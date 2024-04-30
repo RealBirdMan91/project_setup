bfeb8a44781613029ee46ae7c3392b6fa9a17112
RealBirdMan91 <birdman.coding.1991@gmail.com>
2024-04-30 15:37:50 +0200

    trigger approach

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,3 +1,4 @@
+// File: app/admin/page.tsx
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
@@ -6,18 +7,22 @@ import { db } from "@/server/db";
 export default async function Home() {
   const supabase = createServerComponentClient({ cookies });
   const { data } = await supabase.auth.getSession();
-  console.log(data);
   if (!data.session?.user) {
     redirect("/login");
   }
 
-  const notes = await db.note.findMany({
-    where: { userId: data.session.user.id },
+  const profile = await db.profile.findUnique({
+    where: { id: data.session.user.id },
   });
+
+  if (profile?.role !== "admin") {
+    redirect("/");
+  }
+
   return (
     <main>
-      <h1 className="mb-8 text-center text-2xl">Protected page</h1>
-      <pre>{JSON.stringify({ session: data.session, notes }, null, 4)}</pre>
+      <h1 className="mb-8 text-center text-2xl">Admin page</h1>
+      <pre>{JSON.stringify({ profile }, null, 4)}</pre>
     </main>
   );
 }
