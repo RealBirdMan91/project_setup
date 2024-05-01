16c9eba8b56c252c556adede4ca3299df5511c45
RealBirdMan91 <birdman.coding.1991@gmail.com>
2024-05-01 17:28:42 +0200

    polish useActionMutation

--- src/lib/hooks/useActionMutation.tsx
+++ src/lib/hooks/useActionMutation.tsx
@@ -3,7 +3,7 @@ import {
   type ErrorType,
   type SuccessType,
 } from '../types/response'
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 
 type TActionMutation<T> = {
   mutationFn: (data: T) => Promise<ActionResponseType>
@@ -14,35 +14,38 @@ export function useActionMutation<T>({ mutationFn }: TActionMutation<T>) {
   const [error, setError] = useState<Omit<ErrorType, 'type'> | null>(null)
   const [data, setData] = useState<Omit<SuccessType, 'type'> | null>(null)
 
-  const mutate = (data: T) => {
-    setIsPending(true)
-    setError(null)
+  const mutate = useCallback(
+    (data: T) => {
+      setIsPending(true)
+      setError(null)
 
-    mutationFn(data)
-      .then((res: ActionResponseType) => {
-        if (res.type === 'error') {
-          return setError({
-            message: res.message,
-            error: res.error,
-          })
-        }
-        if (res.type === 'success') {
-          setData({
-            data: res.data,
-            message: res.message,
-          })
-        }
-      })
-      .catch((err: Error) => {
-        setError({
-          message: err.message,
-          error: err,
+      mutationFn(data)
+        .then((res: ActionResponseType) => {
+          if (res.type === 'error') {
+            return setError({
+              message: res.message,
+              error: res.error,
+            })
+          }
+          if (res.type === 'success') {
+            setData({
+              data: res.data,
+              message: res.message,
+            })
+          }
         })
-      })
-      .finally(() => {
-        setIsPending(false)
-      })
-  }
+        .catch((err: Error) => {
+          setError({
+            message: err.message,
+            error: err,
+          })
+        })
+        .finally(() => {
+          setIsPending(false)
+        })
+    },
+    [mutationFn],
+  )
 
   return {
     isPending,
